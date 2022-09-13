97de467b77061d31ca29284f11a9e1f1610d42bd
Ehia-BD <ehiaahmedbd@gmail.com>
2022-09-12 19:18:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+                            // Grade system
+
+
+
+var mark=95;
+
+
+if(mark>=90 && mark<=100){
+    console.log('A+')
+}
+else if(mark>=80 && mark<=90){
+    console.log('A')
+}
+else if(mark>=70 && mark<=80){
+    console.log('B')
+}
+else if(mark>=60 && mark<=70){
+    console.log('C')
+}
+else if(mark>=50 && mark<=60){
+    console.log('D')
+}
+else if(mark<=50 ){
+    console.log('fail')
+}
+else{
+    console.log()
+}
+
+
+
+                        // singnal
+
+var singnal='yellow';
+
+
+if(singnal=='Red'){
+    console.log('Ran Danger')
+}
+else if(singnal=='green'){
+    console.log('Ran Start')
+}
+else if(singnal=='yellow'){
+    console.log('Ran Stop')
+}
+else{
+    console.log()
+}
